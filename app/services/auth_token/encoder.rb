9ebb132ca3fac9dbd57310ebee83f426ce8446c8
Olumuyiwa <oluosiname@gmail.com>
2020-04-08 21:14:51 +0200

    add JWT strategy

---
+++ app/services/auth_token/encoder.rb
@@ -0,0 +1,8 @@
+module AuthToken
+  class Encoder
+    def self.call(payload, exp = 24.hours.from_now)
+      payload[:exp] = exp.to_i
+      JWT.encode(payload, ENV['AUTH_TOKEN_SECRET'])
+    end
+  end
+end
