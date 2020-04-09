a4dc5e07bf86fda3f2a2adc7f9a15abde69897c7
Olumuyiwa <oluosiname@gmail.com>
2020-04-09 11:14:40 +0200

    add partner and link to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'callbacks' do
+    describe '#create_partner' do
+      let(:user) { create(:user) }
+
+      it 'creates a new partner' do
+        expect { user.create_partner! }.to change { Partner.count }.by(1)
+        expect(Partner.last.user).to eq(user)
+      end
+    end
+  end
 end
