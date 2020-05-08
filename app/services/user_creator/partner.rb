e38ef86dc509adce9be8e233657e49697976d70c
Olumuyiwa <oluosiname@gmail.com>
2020-05-08 16:57:47 +0200

    create partner with name

--- app/services/user_creator/partner.rb
+++ app/services/user_creator/partner.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 module UserCreator
   class Partner < ApplicationService
-    attr_reader :user
+    attr_reader :user, :partner
 
-    def initialize(user)
+    def initialize(user, partner)
       @user = user
+      @partner = partner
     end
 
     def call
       User.transaction do
         user.save!
-        user.create_partner!
+        user.create_partner!(name: partner[:name])
         Response::Success.new(user, :user)
       end
-
     rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotDestroyed
       Response::Failure.new(user.errors.full_messages)
     end
