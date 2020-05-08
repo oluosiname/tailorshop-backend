aee191390eaf5daa75575f0a5f2d8e18102d2566
Olumuyiwa <oluosiname@gmail.com>
2020-05-08 17:00:48 +0200

    update model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, :password, presence: true
+  validates :email, uniqueness: true
 
   has_one :partner
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'callbacks' do
-    describe '#create_partner' do
-      let(:user) { create(:user) }
+  describe 'validations' do
+    subject { build(:user) }
 
-      it 'creates a new partner' do
-        expect { user.create_partner! }.to change { Partner.count }.by(1)
-        expect(Partner.last.user).to eq(user)
-      end
-    end
+    it { is_expected.to validate_uniqueness_of :email }
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to have_one :partner }
   end
 end
