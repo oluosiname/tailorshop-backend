05e2a657d549c817f63aa25c6ab4706dac50598c
Olumuyiwa <oluosiname@gmail.com>
2020-05-08 16:37:53 +0200

    add name to partner

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class Partner < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_073901) do
+ActiveRecord::Schema.define(version: 2020_05_08_135053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_04_09_073901) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.index ["name"], name: "index_partners_on_name", unique: true
     t.index ["user_id"], name: "index_partners_on_user_id"
   end
 
