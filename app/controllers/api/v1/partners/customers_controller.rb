5fb62fc06a97abf506097bb7dc38fdd7f4227c1d
Olumuyiwa <oluosiname@gmail.com>
2020-08-22 11:21:36 +0200

    paginate

--- app/controllers/api/v1/partners/customers_controller.rb
+++ app/controllers/api/v1/partners/customers_controller.rb
@@ -3,8 +3,8 @@
 class Api::V1::Partners::CustomersController < Api::V1::Partners::BaseController
   def index
     options = {}
-    options[:meta] = { total: 2, has_next_page: true }
-    customers = partner.customers.includes(:addresses)
+    customers = partner.customers.includes(:addresses).page(params[:page])
+    options[:meta] = { has_next_page: customers.next_page.present? }
     render json: CustomerSerializer.new(customers, options).serializable_hash
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,9 @@
 class Customer < ApplicationRecord
   include Uuidable
 
+  paginates_per 12
+  max_paginates_per 30
+
   belongs_to :partner
   has_many :addresses, as: :addressable
 
